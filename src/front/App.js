be0b9c15290437493fb84611580c1d1535fcdadc
Kevin GANEM <Ganemkevin@gmail.com>
2022-03-16 14:53:34 +0100

    Post

--- src/front/App.js
+++ src/front/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState();
+
+  const send = () => {
+    const formData = new FormData();
+    formData.append("image", image);
+    fetch("http://localhost:8000/uploads", {
+      method: "POST",
+      body: formData,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="file"
+        onChange={(event) => setImage(event.target.files[0])}
+      ></input>
+      <button onClick={send}>Send</button>
+    </>
   );
 }
 
